d39e28aa25bb15419e91640437e879a2894accaf
ichenq <ichenq@gmail.com>
2014-05-23 05:49:58 +0800

    add libuv v0.11.25

---
+++ premake4.lua
@@ -0,0 +1,143 @@
+--
+-- Premake4 build script (http://industriousone.com/premake/download)
+--
+
+solution 'myuv'
+    configurations {'Debug', 'Release'}            
+    targetdir 'bin'
+    platforms {"x64"}
+    flags {'ExtraWarnings'}
+    
+    configuration 'Debug'
+        defines { 'DEBUG' }
+        flags { 'Symbols' }
+        
+    configuration 'Release'
+        defines { 'NDEBUG' }
+        flags { 'Symbols', 'Optimize' }
+        
+    project 'unittest'
+        language 'C++'
+        location 'build'
+        kind 'ConsoleApp'
+        uuid "AB7D1C15-7A44-41a7-8864-230D8E345608"
+        
+        defines
+        {
+            "WIN32",
+            "NOMINMAX",
+            "WIN32_LEAN_AND_MEAN",
+            "_WIN32_WINNT=0x0600",
+            "_SCL_SECURE_NO_WARNINGS",
+            "_CRT_SECURE_NO_WARNINGS",
+        }
+        files 
+        {
+            'dep/gtest/src/gtest-all.cc',
+            'test/*.h',
+            'test/*.cpp',
+        }
+        includedirs
+        {
+            "src",
+            "dep/gtest",
+            "dep/gtest/include",
+        }
+
+        links 
+        {
+            "libmyuv",
+            "libuv",
+        }
+        
+    project 'libmyuv'
+        location 'build'
+        kind 'StaticLib'
+        language 'C'
+        uuid "8701594A-72B8-4a6a-AEF3-6B41BBC33E65"
+        
+        defines
+        {
+            "WIN32",
+            "NOMINMAX",
+            "WIN32_LEAN_AND_MEAN",
+            "_WIN32_WINNT=0x0600",
+            "_SCL_SECURE_NO_WARNINGS",
+            "_CRT_SECURE_NO_WARNINGS",
+        }
+        
+        files 
+        {
+            'src/**.h',
+            'src/**.c',
+        }
+        includedirs
+        {
+            "src",
+            "dep/libuv/include",
+            "dep/lua/src",
+        }
+
+    project 'libuv'
+        location 'build'
+        kind 'StaticLib'
+        language 'C'
+        uuid "CEBF9FA1-2A97-4980-83A1-3B4C39AC7AC8"
+        
+        defines
+        {
+            "WIN32",
+            "NOMINMAX",
+            "WIN32_LEAN_AND_MEAN",
+            "_WIN32_WINNT=0x0600",
+            "_SCL_SECURE_NO_WARNINGS",
+            "_CRT_SECURE_NO_WARNINGS",
+        }
+        
+        files 
+        {
+            'dep/libuv/include/tree.h',
+            'dep/libuv/include/uv.h',
+            'dep/libuv/include/uv-errno.h',
+            'dep/libuv/include/uv-version.h',
+            'dep/libuv/include/uv-win.h',
+            'dep/libuv/src/fs-poll.c',
+            'dep/libuv/src/heap-inl.c',
+            'dep/libuv/src/inet.c',
+            'dep/libuv/src/queue.h',
+            'dep/libuv/src/uv-common.h',
+            'dep/libuv/src/uv-common.c',
+            'dep/libuv/src/version.c',
+            'dep/libuv/src/win/*.c',
+        }
+        includedirs
+        {
+            "dep/libuv/include",
+            "dep/libuv/src",
+        }
+        
+    project 'liblua'
+        location 'build'
+        kind 'StaticLib'
+        language 'C'
+        uuid "7C0A496A-CCC3-4e4b-AEB1-4AD192BF7D47"
+        
+        defines
+        {
+            "WIN32",
+            "NOMINMAX",
+            "WIN32_LEAN_AND_MEAN",
+            "_WIN32_WINNT=0x0600",
+            "_SCL_SECURE_NO_WARNINGS",
+            "_CRT_SECURE_NO_WARNINGS",
+        }
+        
+        files 
+        {
+            'dep/lua/src/*.h',
+            'dep/lua/src/*.c',
+        }
+        includedirs
+        {
+            "dep/lua/src",
+        }        
